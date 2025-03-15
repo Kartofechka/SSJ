255ae176bc3a3846d3887c457fad838e8c585e8c
PRYVET <ekartoshkin003@gmail.com>
2025-03-15 15:20:23 +0300

    New files by Potato

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "taskFunctions.h"
+
+int main() {
+    setlocale(LC_ALL, ".1251");
+    int32_t size{};
+    InputSize(size);
+    if (!CheckSize(size)) { 
+        std::cerr << "Некорректный размер массива!\n";
+        return 1;
+    }
+    ChoseTypeOfElement(size);
+    return 0;
+}
 
---
+++ taskFunctions.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#ifndef Task_Functions_H
+#define Task_Functions_H
+
+void InputSize(int32_t&);
+bool CheckSize(int32_t);
+void ChoseTypeOfElement(int32_t);
+int32_t SumOfSimpleNums(int32_t*, int32_t);
+bool IsSimple(int32_t);
+void RemoveNegativesToZeros(int32_t*, int32_t);
+
+#endif
