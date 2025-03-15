ef95527a2fde0da0bd241a398d8314108d4b8e9d
Ilya <zhdilya7@gmail.com>
2025-03-15 15:37:42 +0300

    добавлена функция CountElements которая считает количество элементов между нулями

--- taskFunctions.h
+++ taskFunctions.h
@@ -10,5 +10,48 @@ void PrintArray(T* array, int32_t size) {
 }
 
 
+template<typename T>
+void FindFirstZeroIndex(T* array, int32_t size, int32_t& firstZeroInd);
+
+template<typename T>
+void FindLastZeroIndex(T* array, int32_t size, int32_t& lastZeroInd);
+
+
+template<typename T>
+int32_t CountElements(T* array, int32_t size) {
+	int32_t firstZeroInd{ -1 };
+	int32_t lastZeroInd{ -1 };
+
+	FindFirstZeroIndex(array, size, firstZeroInd);
+	FindLastZeroIndex(array, size, lastZeroInd);
+
+	if (firstZeroInd == lastZeroInd) {
+		return -1;
+	}
+	else {
+		return lastZeroInd - firstZeroInd - 1;
+	}
+}
+
+template<typename T>
+void FindFirstZeroIndex(T* array, int32_t size, int32_t& firstZeroInd) {
+	for (size_t i = 0; i < size; ++i) {
+		if (static_cast<int32_t>(array[i]) == 48 || array[i] == 0) {
+			firstZeroInd = i;
+			break;
+		}
+	}
+}
+
+template<typename T>
+void FindLastZeroIndex(T* array, int32_t size, int32_t& lastZeroInd) {
+	for (int32_t i = (size-1); i > -1; --i) {
+		if (static_cast<int32_t>(array[i]) == 48 || array[i] == 0) {
+			lastZeroInd = i;
+			break;
+		}
+	}
+}
+
 #endif
 
